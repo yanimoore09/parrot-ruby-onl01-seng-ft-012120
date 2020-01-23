920a2479d9f81b3965f585bd5a8aa0a93f5873a3
Yani Moore <yanimoore09@gmail.com>
2020-01-23 07:24:17 +0000

    Done.

--- parrot.rb
+++ parrot.rb
@@ -1,2 +1,9 @@
 # Create method `parrot` that outputs a given phrase and
 # returns the phrase
+
+def method (parrot= pretty bird , bird = noise )
+  phrase = squawk
+  puts ("the sound is a #{parrot}")
+  return squawk 
+
+end
